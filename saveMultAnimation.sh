c7df1d91e2e9417046c43bbc543e22ccb524dcb6
zhijie <zhijie@berkeley.edu>
2018-05-26 21:49:52 -0700

    shell to save animations for H2AZ uH2B WT nucleosome-PolII traces in one session

---
+++ saveMultAnimation.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#to run: ./saveMultAnimation.sh
+
+echo start saving animation ...
+
+echo start saving H2AZ-PolII-movie
+python /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/H2AZ-PolII-movie/*.py /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/H2AZ-PolII-movie/*.pkl --save
+
+echo start saving uH2B-PolII-movie
+python /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/uH2B-PolII-movie/*.py /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/uH2B-PolII-movie/*.pkl --save
+
+echo start saving WT-PolII-movie
+python /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/WT-PolII-movie/*.py /Users/ZHIJIE/Documents/2018\ Paper/Data\ figures/Movies/WT-PolII-movie/*.pkl --save
+
+echo done
