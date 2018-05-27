84340c8abb5cb684ef04a9902834105d8d323661
zhijie <zhijie@berkeley.edu>
2018-05-26 21:15:45 -0700

    updated to change fontsize, fps to 15, downsample to 1, filename to pkl name

--- pkl-animation.py
+++ pkl-animation.py
@@ -1,3 +1,4 @@
+# python animate_pkl.py *.pkl (--save)
 from pathlib import Path
 import mplcursors
 from qtlib.types import OpenFilesType
@@ -10,8 +11,8 @@ def pklAnimation(time, dist, name, save):
     fig = plt.figure()
     ax = plt.axes(xlim=(0, max(time) + 50))
     ax.set(xlabel = "Time (s)", ylabel = "Transcribed distance (bp)", title = f"{name}.pkl")
-    ax.xaxis.label.set_fontsize(14)
-    ax.yaxis.label.set_fontsize(14)
+    # ax.xaxis.label.set_fontsize(14)
+    # ax.yaxis.label.set_fontsize(14)
     time_text = ax.text(0.95, 0.05,'',horizontalalignment='right',
         verticalalignment='bottom',
         transform=ax.transAxes,
@@ -44,7 +45,7 @@ def pklAnimation(time, dist, name, save):
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(time), interval=1, blit=True, repeat=False)
     # save animation
     if save:
-        anim.save('animation_pkl.mp4', writer="ffmpeg", fps=50, metadata={'title':'Animation of ploting ' + name +'.pkl'})
+        anim.save(f"{name}.mp4", writer="ffmpeg", fps=15, metadata={'title':'Animation of ploting ' + name +'.pkl'})
     else:
         plt.show()
 
@@ -52,8 +53,8 @@ def pklNpsAnimation(time, dist, name, save):
     fig = plt.figure()
     ax = plt.axes(xlim=(min(time) - 50, max(time) + 130), ylim=(min(dist) - 50, max(dist) + 50))
     ax.set(xlabel = "Time (s)", ylabel = "Transcribed distance (bp)", title = f"{name}.pkl")
-    ax.xaxis.label.set_fontsize(14)
-    ax.yaxis.label.set_fontsize(14)
+    # ax.xaxis.label.set_fontsize(14)
+    # ax.yaxis.label.set_fontsize(14)
     time_text = ax.text(0.01, 0.98,'',horizontalalignment='left',
         verticalalignment='top',
         transform=ax.transAxes,
@@ -65,42 +66,36 @@ def pklNpsAnimation(time, dist, name, save):
     npss = 888.5, 961.5, 1035.5
     for pos in npss:
         ax.axhline(pos, ls="--", color="k")
-    ax.text(580, 880, 'NPS Entry')
-    ax.text(580, 952, 'dyad')
-    ax.text(580, 1026, 'NPS Exit')
+    ax.text(533, 876, 'NPS Entry')
+    ax.text(533, 948, 'dyad')
+    ax.text(533, 1022, 'NPS Exit')
 
     def init():
         line.set_data([], [])
         time_text.set_text('Start')
-        # pos_text.set_text('NPS Entry')
         return line, time_text
 
     def animate(i):
         x = time[i]
         y = dist[i]
-
         xdata.append(x)
         ydata.append(y)
         line.set_data(xdata, ydata)
-
         time_text.set_text('Time: %.2fs' % x)
         return line, time_text
 
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(time), interval=1, blit=True, repeat=False)
     # save animation
     if save:
-        anim.save('animation_pkl_nps.mp4', writer="ffmpeg", fps=50, metadata={'title':'Animation of ploting ' + name +'.pkl'})
+        anim.save(f"{name}-NPS.mp4", writer="ffmpeg", fps=15, metadata={'title':'Animation of ploting ' + name +'.pkl'})
     else:
         plt.show()
 
 def main(filePath: Path, *,
          save: bool = False):
-
     print("reading...", filePath)
-
     name = str(filePath)[:-4]
-
-    tr = tz.trace(filePath).get_downsampled_to(10)
+    tr = tz.trace(filePath).get_downsampled_to(1)
     time = tr.time.tolist()
     dist = tr.dist.tolist()
 
@@ -108,14 +103,12 @@ def main(filePath: Path, *,
     endIndex = next(n for n, i in enumerate(dist) if i > 1050)
     print(startIndex, endIndex)
 
-    font = {'family' : 'sans-serif',
-        'size'   : 14}
+    font = {'family':'sans-serif', 'size': 12}
     plt.rc('font', **font)
-
+# creat animation for full trace or zoom in animation on NPS region
     pklAnimation(time, dist, name, save)
     pklNpsAnimation(time[startIndex:endIndex], dist[startIndex:endIndex], name, save)
 
 if __name__ == "__main__":
-
     import defopt
     defopt.run(main)
